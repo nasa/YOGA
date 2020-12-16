5f423fc1d131a673e7ea063ca2992584e8fb32ab
Cindy Zhang <xinyu.zhang@nasa.gov>
2020-12-16 10:38:24 -0500

    Use one-ring-comp for one-ring submodule

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-subpacks=${1:-"build-tools pancake parfait MessagePasser tracer t-infinity"}
+subpacks=${1:-"build-tools pancake one-ring parfait MessagePasser tracer t-infinity"}
 
 for i in $subpacks
 do
@@ -14,44 +14,4 @@ do
   fi
 done
 
-git submodule update --init --no-fetch -- one-ring
 
-# Sparse Checkout of one-ring
-
-# Find .git directory
-git_dir=`git rev-parse --git-dir`
-
-# Check for one-ring submodule
-if test -z "${git_dir}/modules/one-ring"
-then
-  echo "Failed to find one-ring submodule description!"
-  exit 1
-fi
-
-# This is the subset of files needed to support the distance function
-cat > ${git_dir}/modules/one-ring/info/sparse-checkout << EOF
-yoga/src
-nanoflann/include/nanoflann.hpp
-ddata/ddata/*.h*
-#t-infinity/src/base/t-infinity/*.h
-#t-infinity/src/t-infinity-interfaces/TinfMesh.h
-#t-infinity/src/t-infinity-runtime/t-infinity/PluginLoader.h
-#t-infinity/src/t-infinity-runtime/t-infinity/Communicator.h
-#t-infinity/src/t-infinity-runtime/t-infinity/Mesh.h
-#t-infinity/src/t-infinity-runtime/t-infinity/MeshLoader.h
-#t-infinity/src/t-infinity-runtime/t-infinity/RepartitionerInterface.h
-#t-infinity/src/t-infinity-runtime/t-infinity/VizFactory.h
-#t-infinity/src/t-infinity-runtime/t-infinity/VectorFieldAdapter.h
-#t-infinity/src/t-infinity-runtime/t-infinity/VizPlugin.h
-EOF
-
-(cd one-ring
- git config core.sparseCheckout true
- git checkout --
-)
-(cd src/ddata
- ./relink-files.sh
-)
-(cd src/yoga
- ./relink-files.sh
-)
